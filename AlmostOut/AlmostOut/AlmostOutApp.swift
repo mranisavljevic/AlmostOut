85dc4c2816dc5c9e9cc3b6dcd662c05189f3b7cb
Miles Ranisavljević <itstop3@gmail.com>
2025-08-11 12:18:14 -0700

    chore: Add initial app folder and files

---
+++ AlmostOut/AlmostOut/AlmostOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlmostOutApp.swift
+//  AlmostOut
+//
+//  Created by Miles Ranisavljevic on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AlmostOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlmostOut/AlmostOutTests/AlmostOutTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlmostOutTests.swift
+//  AlmostOutTests
+//
+//  Created by Miles Ranisavljevic on 8/11/25.
+//
+
+import Testing
+@testable import AlmostOut
+
+struct AlmostOutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
