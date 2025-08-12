31678637b88ab0b05adfb8fae06e8249963d9970
Miles Ranisavljević <itstop3@gmail.com>
2025-08-12 12:52:50 -0700

    chore: Fix issue with test target configuration

--- AlmostOut/AlmostOutTests/AuthViewModelTests.swift
+++ AlmostOut/AlmostOutTests/AuthViewModelTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import AlmostOut
 
 @MainActor
 final class AuthViewModelTests: XCTestCase {
--- AlmostOut/AlmostOutTests/PriceGuidanceTests.swift
+++ AlmostOut/AlmostOutTests/PriceGuidanceTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import AlmostOut
 
 class PriceGuidanceTests: XCTestCase {
     
